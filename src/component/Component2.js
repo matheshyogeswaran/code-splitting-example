29c86dc3410f4b4bc0f2b7ed0ab0d82caa75ae6c
matheshyogeswaran <matheshyogeswaran@gmail.com>
2024-09-07 10:24:11 +0530

    section 1 done

---
+++ src/component/Component2.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Component2() {
+  
+  const numbers = Array.from({ length: 1000 }, (_, i) => i + 1);
+
+  return (
+    <div>
+      <h1>Numbers from 1 to 1000</h1>
+      <ul>
+        {numbers.map(number => (
+          <li key={number}>{number}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
